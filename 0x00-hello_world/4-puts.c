76471657077fbcba86451f699ea4f408cea8de95
softorch <websoftorch>
2023-02-18 12:47:11 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A program that prints a line with put function
+ * Return: 0 (This program returns 0 to indicate success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
