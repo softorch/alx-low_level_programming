fcced840c8ee31bb658e87c157c6ece43da4cf3d
Uchenna Nzenwata <websoftorch@gmail.com>
2023-04-08 13:04:30 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @x: first number
+ * @y: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int x, unsigned long int y)
+{
+	int i, count = 0;
+	unsigned long int curnt;
+	unsigned long int exclusive = x ^ y;
+
+	for (i = 63; i >= 0; i--)
+	{
+		curnt = exclusive >> i;
+		if (curnt & 1)
+			count++;
+	}
+
+	return (count);
+}
+
